430dc5b0b6897fa069012aeafaea2d7a176cc37e
dgalus <dawid_galus@wp.pl>
2019-02-01 00:13:47 +0100

    instrument, title and file models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,4 @@
+from .file import Files
+from .instrument import Instrument
+from .title import Title
 from .user import User
--- app/models/instrument.py
+++ app/models/instrument.py
@@ -2,12 +2,11 @@ from app import db
 import datetime
 import uuid
 
-class Files(db.Model):
-    __tablename__ = 'files'
+class Instrument(db.Model):
+    __tablename__ = 'instrument'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     public_id = db.Column(db.String, unique=True, nullable=False)
-    filename = db.Column(db.String, unique=True, nullable=False)
-    original_filename = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
     created_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     created_on = db.Column(db.DateTime, nullable=False)
     modified_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
@@ -16,10 +15,9 @@ class Files(db.Model):
     deleted_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
     deleted_on = db.Column(db.DateTime, nullable=True)
 
-    def __init__(self, filename, original_filename, created_by, created_on):
+    def __init__(self, name, created_by, created_on):
         self.public_id = str(uuid.uuid4())
-        self.filename = filename
-        self.original_filename = original_filename
+        self.name = name
         self.created_by = created_by
         self.created_on = datetime.datetime.now()
         self.modified_by = None
